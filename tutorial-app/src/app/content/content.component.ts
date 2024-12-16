98ff2ee1a57db1e55b06aa9ae9fb682519e588df
VanOps <ivan.gomez859@comunidadunir.net>
2024-12-16 18:36:29 +0100

    chore: config content format

--- tutorial-app/src/app/content/content.component.ts
+++ tutorial-app/src/app/content/content.component.ts
@@ -13,7 +13,7 @@ export class ContentComponent {
   content: any = {
     "Seccion1": {
       "title": "Introducción",
-      "content": "Angular es un framework de desarrollo de aplicaciones web de código abierto mantenido por Google. Está diseñado para facilitar la creación de aplicaciones web dinámicas y de una sola página (SPA, por sus siglas en inglés). Aquí tienes una breve introducción a sus características y componentes clave:"
+      "content": "Angular es un framework de desarrollo de aplicaciones web de código abierto mantenido por Google. Está diseñado para facilitar la creación de aplicaciones web dinámicas y de una sola página (SPA, por sus siglas en inglés). Aquí encontrarás una breve introducción a sus características y componentes clave."
     },
     "Seccion2": {
       "title": "Características Principales de Angular",
@@ -33,9 +33,9 @@ export class ContentComponent {
       "content": [
         "Para empezar a trabajar con Angular, necesitas tener Node.js y npm instalados en tu sistema. Puedes descargar e instalar Node.js desde su sitio web oficial.",
         "Una vez que tengas Node.js instalado, puedes instalar Angular CLI (Command Line Interface) utilizando npm. Angular CLI es una herramienta de línea de comandos que facilita la creación, el desarrollo y la implementación de aplicaciones Angular.",
-        "Para instalar Angular CLI, abre una terminal y ejecuta el siguiente comando: `npm install -g @angular/cli`",
-        "Una vez que Angular CLI esté instalado, puedes crear un nuevo proyecto Angular utilizando el siguiente comando: `ng new my-app`",
-        "Este comando creará una nueva carpeta llamada my-app con la estructura inicial de un proyecto Angular. Para iniciar el servidor de desarrollo y ver la aplicación en tu navegador, puedes ejecutar el siguiente comando: `cd my-app && ng serve --open",
+        "Para instalar Angular CLI, abre una terminal y ejecuta el siguiente comando: <code>npm install -g @angular/cli</code>",
+        "Una vez que Angular CLI esté instalado, puedes crear un nuevo proyecto Angular utilizando el siguiente comando: <code>ng new my-app</code>",
+        "Este comando creará una nueva carpeta llamada my-app con la estructura inicial de un proyecto Angular. Para iniciar el servidor de desarrollo y ver la aplicación en tu navegador, puedes ejecutar el siguiente comando: <code>cd my-app && ng serve --open</code>",
         "Esto iniciará el servidor de desarrollo y abrirá la aplicación en tu navegador predeterminado. Ahora puedes empezar a desarrollar tu aplicación Angular y ver los cambios en tiempo real."
       ]
     },
@@ -43,16 +43,28 @@ export class ContentComponent {
       "title": "Creacion de este proyecto",
       "content": [
         "Este proyecto fue creado utilizando Angular CLI. Para crear este proyecto, se ejecutaron los siguientes comandos:",
-        "`ng new tutorial-app` para crear un nuevo proyecto Angular.",
-        "`ng generate component head` para generar un componente llamado HeadComponent.",
-        "`ng generate component content` para generar un componente llamado ContentComponent.",
+        "<code>ng new tutorial-app</code>` para crear un nuevo proyecto Angular.",
+        "<code>ng generate component head</code>` para generar un componente llamado HeadComponent.",
+        "<code>ng generate component content</code>` para generar un componente llamado ContentComponent.",
         "Estos comandos generaron la estructura inicial del proyecto y los componentes necesarios para mostrar el contenido de la aplicación. Luego, se modificaron los archivos HTML y CSS de los componentes para personalizar el diseño y el estilo de la aplicación.",
         "Finalmente, se agregó el contenido de la aplicación en el archivo content.component.ts y se utilizó la directiva *ngFor en el archivo content.component.html para mostrar dinámicamente el contenido de cada sección."
       ]
     },
     "Seccion5": {
       "title": "Descripcion del código este proyecto",
-      "content": "This is the about content"
+      "paragraph": [
+        "Este proyecto utiliza componentes para organizar la interfaz de usuario y mostrar el contenido de la aplicación. Cada componente tiene su propio archivo HTML, CSS y TypeScript para definir la estructura, el estilo y la lógica del componente.",
+        "El componente content.component define la clase ContentComponent que contiene el contenido de la aplicación en forma de objetos JSON. Cada objeto JSON representa una sección de la aplicación con un título y un contenido asociado.",
+        "El archivo content.component.html utiliza la directiva *ngFor para iterar sobre los objetos JSON y mostrar dinámicamente el contenido de cada sección en la página.",
+        "El componente head.component define la clase HeadComponent que muestra el encabezado de la aplicación con el título principal. El título se muestra utilizando la interpolación de datos en el archivo head.component.html.",
+        "El componente wellcome-message.component define la clase WellcomeMessageComponent que muestra un mensaje de bienvenida en la página de inicio de la aplicación. El mensaje se muestra utilizando la interpolación de datos en el archivo wellcome-message.component.html.",
+        "El archivo app.component.html utiliza los componentes HeadComponent y ContentComponent para mostrar el encabezado y el contenido de la aplicación en la página principal.",
+        "El archivo app.component.css contiene los estilos CSS para personalizar el diseño de la aplicación, incluyendo el color de fondo, el tamaño de fuente y el espaciado de los elementos.",
+        "El archivo app.module.ts define el módulo raíz de la aplicación que importa los componentes y módulos necesarios para ejecutar la aplicación. El módulo raíz también define el componente principal de la aplicación y lo declara en la lista de bootstrapComponents.",
+        "El archivo main.ts inicia la aplicación Angular llamando a la función platformBrowserDynamic().bootstrapModule(AppModule) para cargar el módulo raíz de la aplicación en el navegador.",
+        "Este proyecto también utiliza el módulo CommonModule de Angular para importar las directivas y pipes comunes utilizados en la aplicación. El módulo CommonModule proporciona funcionalidades básicas como ngIf y ngFor que son necesarias para la aplicación.",
+        "En resumen, este proyecto utiliza componentes, directivas y módulos de Angular para crear una aplicación web dinámica y de una sola página. Los componentes se utilizan para organizar la interfaz de usuario y mostrar el contenido de la aplicación, mientras que las directivas se utilizan para manipular el DOM y aplicar lógica condicional."
+      ]
     }
   };
 
