cb97538322769cd00578078d272d26375028fe86
VanOps <ivan.gomez859@comunidadunir.net>
2024-12-21 09:23:28 +0100

    chore: message dissapear in 2 seconds

--- tutorial-app/src/app/welcome-message/welcome-message.component.ts
+++ tutorial-app/src/app/welcome-message/welcome-message.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-welcome-message',
@@ -8,8 +7,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './welcome-message.component.html',
   styleUrls: ['./welcome-message.component.css']
 })
-export class WelcomeMessageComponent {
+export class WelcomeMessageComponent implements OnInit {
   showAlert = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showAlert = false;
+    }, 2000); // 2000 milliseconds = 2 seconds
+  }
 
   closeAlert() {
     this.showAlert = false;
